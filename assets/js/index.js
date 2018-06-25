69d888b17fa3ddf24bb999f8d595acd05b188aef
看着黎明丶庆幸 <xanwidtf@foxmail.com>
2018-06-25 23:57:57 +0800

    add index.js test

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+    //mytimeline黑夜模式，根据时间23--7点设为白色字体,用定时器5s判断一次
+    var date = new Date();
+    var hour = date.getHours();
+        if(hour==23||hour<7)
+    {
+        var aList=document.getElementsByClassName("archive-name");
+        for(var i=0;i<aList.length;i++){
+            document.getElementsByClassName("archive-name")[i].style.color="#fff";
+        }
+        
+    }else{
+        var aList=document.getElementsByClassName("archive-name");
+        for(var i=0;i<aList.length;i++){
+            document.getElementsByClassName("archive-name")[i].style.color="#333";
+        }
+    }
+}
