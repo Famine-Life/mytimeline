009a36e83861f4b6eae764aa772557a8d72765d9
看着黎明丶庆幸 <xanwidtf@foxmail.com>
2018-06-26 00:09:08 +0800

    黑夜模式修复done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-    //mytimeline黑夜模式，根据时间23--7点设为白色字体,用定时器5s判断一次
+    //mytimeline黑夜模式，根据时间23--7点设为白色字体
     var date = new Date();
     var hour = date.getHours();
         if(hour==23||hour<7)
